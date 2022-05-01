fffaa097741b37d1213db945d454922f86040a0b
Amir Elkassar <ae.mescogroup@gmail.com>
2022-05-01 13:26:06 +0200

    Accounts Controller added

---
+++ GLSPM.Application/Dtos/Identity/RegisterUserDto.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLSPM.Application.Dtos.Identity
+{
+    public class RegisterUserDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public IFormFile? Avatar { get; set; }
+    }
+
+    public class RegisterUserDtoValidator:AbstractValidator<RegisterUserDto>
+    {
+        public RegisterUserDtoValidator()
+        {
+            RuleFor(u=>u.Username)
+                .NotEmpty()
+                .MinimumLength(4);
+
+            RuleFor(u => u.Password)
+                .NotEmpty()
+                .MinimumLength(8);
+
+            RuleFor(u => u.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            When(u => u.Avatar != null, () =>
+            {
+                RuleFor(u => u.Avatar.Length)
+                .ExclusiveBetween(1000, 5000000);
+            });
+        }
+    }
+}
---
+++ GLSPM.Server/Controllers/AccountsController.cs
@@ -0,0 +1,112 @@
+﻿using GLSPM.Application.AppServices.Interfaces;
+using GLSPM.Application.Dtos;
+using GLSPM.Application.Dtos.Identity;
+using GLSPM.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using HeyRed.Mime;
+namespace GLSPM.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ILogger<AccountsController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IAuthenticationAppService _authenticationAppService;
+
+        public AccountsController(ILogger<AccountsController> logger,
+            UserManager<ApplicationUser> userManager,
+            IAuthenticationAppService authenticationAppService)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _authenticationAppService = authenticationAppService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost("Login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Login([FromBody] LoginUserDto input)
+        {
+            _logger.LogInformation("Attimpting to login a user...");
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning("invalid model passed.");
+                var response = new SingleObjectResponse<object>
+                {
+                    Success = false,
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Message = "invalid model passed.",
+                    Error = ModelState
+                };
+                return BadRequest(response);
+            }
+            if (!await _authenticationAppService.ValidateUser(input))
+            {
+                _logger.LogWarning("User not found");
+                var response = new SingleObjectResponse<object>
+                {
+                    Success = false,
+                    StatusCode = StatusCodes.Status404NotFound,
+                    Message = "User not found",
+                    Error = "incorrect username or password"
+                };
+                return NotFound(response);
+            }
+            else
+            {
+                _logger.LogInformation("User logged");
+                _logger.LogInformation("Attempting to preparing the response...");
+                //preparing the user data
+                var authUser = _authenticationAppService.User;
+                var response = new LoginResponseDto()
+                {
+                    UserID = authUser.Id,
+                    Email = authUser.Email,
+                    Username = authUser.UserName,
+                    Avatar = Url.Action(nameof(UserAvatar), "Accounts", new { userid = authUser.Id }, Request.Scheme)
+                };
+                response.Roles = await _userManager.GetRolesAsync(authUser);
+                response.IsAppAdmin = response.Roles.Contains("Admin");
+                _logger.LogInformation("User data is ready");
+                //preparing and setting the token model
+                var tokenmodel = await _authenticationAppService.CreateUserToken();
+                response.Token = tokenmodel.Token;
+                response.TokenExpirationDate = tokenmodel.Expiration;
+                _logger.LogInformation("Token is ready");
+                return Ok(response);
+            }
+        }
+
+        [Authorize]
+        [HttpGet("UserAvatar/{userID}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> UserAvatar(string userID)
+        {
+            var user = await _userManager.FindByIdAsync(userID);
+            if (user != null)
+            {
+                user.ImagePath ??= Path.GetFullPath("./Files/Imgs/Userimage.png");
+                return PhysicalFile(user.ImagePath, MimeTypesMap.GetMimeType(user.ImagePath), $"avatar{Path.GetExtension(user.ImagePath)}");
+            }
+            return NotFound(new SingleObjectResponse<object>
+            {
+                Success = false,
+                StatusCode = StatusCodes.Status404NotFound,
+                Message = "User not found",
+                Error = "Couldn't find a user related to the passed id"
+            });
+        }
+    }
+}
